68b6e265e0bf44db5faa445af5de0535f3df3c51
amit yadav <amityadav737653@gmail.com>
2026-05-04 08:24:13 +0530

    end commit

--- 1.js
+++ 1.js
@@ -1,38 +1,4 @@
-// document.getElementById("astroForm").addEventListener("submit", function(e) {
-//   e.preventDefault();
 
-//   let fname = document.getElementById("fname").value;
-//   let lname = document.getElementById("lname").value;
-//   let date = parseInt(document.getElementById("date").value);
-//   let month = parseInt(document.getElementById("month").value);
-
-//   let zodiac = getZodiac(date, month);
-
-//   let message = `
-//     🌟 Hello ${fname} ${lname}! <br><br>
-//     Your Zodiac Sign is <b>${zodiac}</b> <br><br>
-//     🔮 Today’s Prediction: <br>
-//     You will experience positive energy and growth. Stay focused and trust your instincts!
-//   `;
-
-//   document.getElementById("result").innerHTML = message;
-// });
-
-
-// function getZodiac(day, month) {
-//   if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) return "Aries ♈";
-//   if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) return "Taurus ♉";
-//   if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) return "Gemini ♊";
-//   if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) return "Cancer ♋";
-//   if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) return "Leo ♌";
-//   if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) return "Virgo ♍";
-//   if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) return "Libra ♎";
-//   if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) return "Scorpio ♏";
-//   if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) return "Sagittarius ♐";
-//   if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) return "Capricorn ♑";
-//   if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) return "Aquarius ♒";
-//   if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) return "Pisces ♓";
-// }
 
 
 
@@ -181,7 +147,7 @@ form.addEventListener('submit',(event)=>{
 
     const result = document.getElementById('result')
 
-    const first_message = `Hell ${name} ${lname}.`;
+    const first_message = `Hello ${name} ${lname}.`;
     const second_message = `Your Zodiac sign is ${zodiacSigns[month-1]}.`;
     const third_message = compliments[day-1];
 
